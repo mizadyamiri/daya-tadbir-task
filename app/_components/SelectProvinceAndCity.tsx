1385bb316cec7ac34e86d690f83f53cf711caad8
mojtaba Izady <mojtabaizady99@gmail.com>
2025-07-16 13:13:48 +0330

    feat: add checkout page

---
+++ app/_components/SelectProvinceAndCity.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useState } from "react";
+import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import allProvinces from "@/app/_libs/allProvinces.json";
+import allCities from "@/app/_libs/allCities.json";
+
+export default function SelectProvinceAndCity() {
+  const [provinceId, setProvinceId] = useState<string>("");
+  const [cityId, setCityId] = useState<string>("");
+
+  const selectedProvinceCities = allCities.filter(
+    city => city.province_id.toString() === provinceId
+  );
+
+  return (
+    <>
+      <FormControl fullWidth margin='dense' variant='outlined' required>
+        <InputLabel id='province'>استان</InputLabel>
+        <Select
+          id='province'
+          label='استان'
+          value={provinceId}
+          onChange={e => setProvinceId(String(e.target.value))}
+        >
+          {allProvinces.map(province => (
+            <MenuItem key={province.id} value={province.id}>
+              {province.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl fullWidth margin='dense' variant='outlined' required>
+        <InputLabel id='city'>شهرستان</InputLabel>
+        <Select
+          id='city'
+          label='شهرستان'
+          value={cityId}
+          onChange={e => setCityId(String(e.target.value))}
+        >
+          {selectedProvinceCities.map(city => (
+            <MenuItem key={city.id} value={city.id}>
+              {city.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </>
+  );
+}
---
+++ app/checkout/page.tsx
@@ -0,0 +1,87 @@
+import { Paper, Container, Typography, Box, TextField, Button } from "@mui/material";
+import SelectProvinceAndCity from "@/app/_components/SelectProvinceAndCity";
+
+export default function Page() {
+  return (
+    <Container maxWidth='xs' sx={{ padding: { lg: "0px !important" }, my: 6 }}>
+      <form>
+        <Paper
+          elevation={8}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            p: 4,
+          }}
+        >
+          <Typography variant='h1' fontSize={{ md: 50, xs: 30 }} gutterBottom>
+            تکمیل خرید
+          </Typography>
+
+          <Box width={1}>
+            <TextField
+              id='firstname'
+              label='نام'
+              variant='outlined'
+              type='text'
+              required
+              fullWidth
+              margin='dense'
+            />
+            <TextField
+              id='lastname'
+              label='نام خانوادگی'
+              variant='outlined'
+              type='text'
+              required
+              fullWidth
+              margin='dense'
+            />
+            <TextField
+              id='mobile'
+              label='موبایل'
+              variant='outlined'
+              type='number'
+              required
+              fullWidth
+              margin='dense'
+            />
+            <TextField
+              id='postalnum'
+              label='کد پستی'
+              variant='outlined'
+              type='number'
+              required
+              fullWidth
+              margin='dense'
+            />
+
+            <SelectProvinceAndCity />
+
+            <TextField
+              id='address'
+              label='آدرس'
+              variant='outlined'
+              type='text'
+              multiline
+              maxRows={3}
+              required
+              fullWidth
+              margin='dense'
+            />
+          </Box>
+
+          <Button
+            type='submit'
+            variant='contained'
+            fullWidth
+            sx={{ fontSize: 18, mt: 5 }}
+          >
+            پرداخت
+          </Button>
+        </Paper>
+      </form>
+    </Container>
+  );
+}
