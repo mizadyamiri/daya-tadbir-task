8c40d6a96d7d09a2aa5b819b4e2e9880a92ec015
mojtaba Izady <mojtabaizady99@gmail.com>
2025-08-07 10:42:06 +0330

    fix: create nav bar with mui's AppBar component

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -1,47 +1,31 @@
 import Container from '@mui/material/Container';
-import Stack from '@mui/material/Stack';
 import MuiLink from './MuiLink';
 import Image from 'next/image';
 import dayatadbirlogo from '@/public/images/daya-tadbir-logo.png';
 import { getSession } from '@/app/_libs/auth';
 import NavbarButtons from './NavbarButtons';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 
 export default async function Navbar() {
   const session = await getSession();
 
   return (
-    <header>
-      <nav>
-        <Stack
-          alignItems={'center'}
-          justifyContent={'center'}
-          sx={{
-            width: 1,
-            height: 80,
-            bgcolor: '#fff',
-            position: 'sticky',
-            top: 0,
-            zIndex: 999,
-          }}
-        >
-          <Container maxWidth="lg" sx={{ padding: { lg: '0px !important' } }}>
-            <Stack direction="row" alignItems="center" justifyContent="space-between">
-              <MuiLink href="/">
-                <Image
-                  src={dayatadbirlogo}
-                  width={120}
-                  style={{ height: 'auto' }}
-                  alt="daya tadbir logo"
-                />
-              </MuiLink>
+    <AppBar position="fixed" color="secondary">
+      <Container maxWidth="lg" sx={{ padding: { lg: '0px !important' } }}>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <MuiLink href="/">
+            <Image
+              src={dayatadbirlogo}
+              width={120}
+              style={{ height: 'auto' }}
+              alt="daya tadbir logo"
+            />
+          </MuiLink>
 
-              <Stack direction="row" alignItems={'center'}>
-                <NavbarButtons session={session} />
-              </Stack>
-            </Stack>
-          </Container>
-        </Stack>
-      </nav>
-    </header>
+          <NavbarButtons session={session} />
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
