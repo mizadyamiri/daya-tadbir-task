f915e073dcc95acbcf813734c85aed4b46f61a87
mojtaba Izady <mojtabaizady99@gmail.com>
2025-08-14 15:37:22 +0330

    feat: add axios

--- app/_libs/data-services.ts
+++ app/_libs/data-services.ts
@@ -1,3 +1,12 @@
+import axios, { AxiosError } from 'axios';
+import { notFound } from 'next/navigation';
+
+const client = axios.create({
+  baseURL: 'https://fakestoreapi.com',
+  timeout: 8000,
+  headers: { Accept: 'application/json' },
+});
+
 export interface Product {
   id: number;
   title: string;
@@ -9,26 +18,24 @@ export interface Product {
 
 export async function getProducts(): Promise<Product[]> {
   try {
-    const res = await fetch('https://fakestoreapi.com/products');
-    if (!res.ok) throw new Error(`${res.status}, ${res.statusText}`);
-
-    const data = await res.json();
-    return data;
-  } catch (err) {
-    console.error(err);
-    throw err;
+    const res = await client.get<Product[]>('/products');
+    return res.data;
+  } catch (e) {
+    const error = e as AxiosError;
+    if (error.status === 404) notFound();
+    console.error(error);
+    throw error;
   }
 }
 
 export async function getProduct(id: number | string): Promise<Product> {
   try {
-    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
-    if (!res.ok) throw new Error(`${res.status}, ${res.statusText}`);
-
-    const data = await res.json();
-    return data;
-  } catch (err) {
-    console.error(err);
-    throw err;
+    const res = await client.get<Product>(`/products/${id}`);
+    return res.data;
+  } catch (e) {
+    const error = e as AxiosError;
+    if (error.status === 404) notFound();
+    console.error(error);
+    throw error;
   }
 }
